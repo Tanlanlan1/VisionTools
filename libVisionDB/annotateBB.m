f68d5521f641e3084bbbca874dc0f3b8a021a56d
Sangdon Park <sangdonp@LVN403-1.cis.upenn.edu>
2014-07-08 17:54:19 -0400

    add bounding box annotation tools

--- libVisionDB/annotateBB.m
+++ libVisionDB/annotateBB.m
@@ -1,28 +1,127 @@
-%% inputss
-srcImgs = '~/UPenn/Dropbox/img';
-annDir = srcImgs;
-imgExt = 'jpg';
+% %% inputss
+% srcImgs = '~/UPenn/Dropbox/img';
+% annDir = srcImgs;
+% imgExt = 'jpg';
 
+function annotateBB(srcImgDir, annDir, imgExt)
 
-%% 
-hFig = 52323;
-imgList = dir(sprintf('%s/*.%s', srcImgs, imgExt));
-for iInd=1:numel(imgList)
-    [~, fid, ~] = fileparts(imgList(iInd).name);
-    annfn = sprintf('%s/%s_bb.mat', annDir, fid);
+% for efficiency
+imgList = dir(sprintf('%s/*.%s', srcImgDir, imgExt));
+
+% pop-up the figure
+h = figure('Name', 'Annotator', 'CreateFcn', @(src, evt) initFig(src, evt, srcImgDir, annDir, imgList), 'KeyPressFcn', @(obj, evt) handleKeyEvent(obj, evt, srcImgDir, annDir, imgList));
     
-    figure(hFig); clf;
-    imagesc(imread(sprintf('%s/%s', srcImgs, imgList(iInd).name)));
-    title(sprintf('%s (save: double click, discard: close window)', imgList(iInd).name));
-    rect = [];
-    if exist(annfn, 'file')
-        load(annfn);
-        rect = wait(imrect(gca, rect)); % [xmn ymin width height]
-    else
-        rect = wait(imrect); % [xmn ymin width height]
-    end
-    if isempty(rect)
-        continue;
-    end
-    save(annfn, 'rect');
+% wait
+uiwait(h);
+    
+end
+
+function showImg(img, name)
+clf;
+imagesc(img);
+title(sprintf('fn: %s ([f]inish/[n]ext/[p]rev/ne[w]/[s]ave)', name), 'Interpreter', 'none');
+end
+
+function showAnns(anns, imSz)
+global hRects;
+for aInd=1:numel(anns)
+    hRects = [hRects; imrect(gca, anns(aInd).xywh)]; % [xmn ymin width height]
+    textPos = anns(aInd).xywh(1:2);
+    textPos(2) = imSz(1)-textPos(2);
+%     textPos(2) = textPos(2) + 0.05;
+    annotation('textbox', [textPos./imSz([2, 1]) 0.0 0.0], 'String', anns(aInd).label, 'Color', 'r');
+end
+end
+
+function initFig(src, evt, srcImgDir, annDir, imgList)
+global iInd;
+global aInd;
+global anns;
+
+iInd = 1;
+
+[~, fid, ~] = fileparts(imgList(iInd).name);
+annfn = sprintf('%s/%s_bb.mat', annDir, fid);
+if exist(annfn, 'file')
+    load(annfn);
+else
+    anns = struct('label', {}, 'xywh', {});
+end
+aInd = 1;
+
+% show the img
+img = imread(sprintf('%s/%s', srcImgDir, imgList(iInd).name));
+showImg(img, imgList(iInd).name);
+% show anns
+showAnns(anns, size(img)); 
+end
+
+function handleKeyEvent(obj, evt, srcImgDir, annDir, imgList)
+global iInd;
+global aInd;
+global anns;
+global hRects;
+
+switch evt.Key
+    case 'f' % finish annotation
+        close(obj);
+    case 'n' % show next image
+        iInd = min(iInd+1, numel(imgList));
+        
+        [~, fid, ~] = fileparts(imgList(iInd).name);
+        annfn = sprintf('%s/%s_bb.mat', annDir, fid);
+        if exist(annfn, 'file')
+            load(annfn);
+        else
+            anns = struct('label', {}, 'xywh', {});
+        end
+        aInd = 1;
+        hRects = [];
+        
+        % show the img
+        img = imread(sprintf('%s/%s', srcImgDir, imgList(iInd).name));
+        showImg(img, imgList(iInd).name);
+        % show anns
+        showAnns(anns, size(img)); 
+        
+    case 'p' % show previous image
+        
+        iInd = max(iInd-1, 1);
+        
+        [~, fid, ~] = fileparts(imgList(iInd).name);
+        annfn = sprintf('%s/%s_bb.mat', annDir, fid);
+        if exist(annfn, 'file')
+            load(annfn);
+        else
+            anns = struct('label', {}, 'xywh', {});
+        end
+        aInd = 1;
+        hRects = [];
+        
+        % show the img
+        img = imread(sprintf('%s/%s', srcImgDir, imgList(iInd).name));
+        showImg(img, imgList(iInd).name);
+        % show anns
+        showAnns(anns, size(img)); 
+        
+    case 's' % save annotations
+        
+        [~, fid, ~] = fileparts(imgList(iInd).name);
+        annfn = sprintf('%s/%s_bb.mat', annDir, fid);
+        save(annfn, 'anns');
+                
+    case 'w' % draw new annotation
+        
+        % draw
+        hRect = imrect; % [xmn ymin width height]
+        hRects = [hRects hRect];
+        
+        % get the label name
+        curLabel = input('input label > ', 's');
+        
+        % save
+        aInd = aInd + 1;
+        anns(aInd).label = curLabel;
+        anns(aInd).xywh = getPosition(hRect);
+end
 end
