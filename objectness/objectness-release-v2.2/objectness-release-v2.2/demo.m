25809de8ad2d7c1b2bf87ecfb9206c2462c99b1a
Sangdon Park <ggdons@sangdons-mbp.wireless.yale.internal>
2014-08-30 13:41:55 -0400

    add objectness libs

---
+++ objectness/objectness-release-v2.2/objectness-release-v2.2/demo.m
@@ -0,0 +1,14 @@
+% imgExample = imread('002053.jpg');
+im = imread('test1.png');
+boxes = runObjectness(im,100);
+figure,imshow(im),drawBoxes(boxes);
+
+for i=1:size(boxes,1)
+    bbox = boxes(i, :);
+    figure(2);
+    imshow(im);
+    hold on; 
+    rectangle('Position', [bbox(2) bbox(1) bbox(4)-bbox(2) bbox(3)-bbox(1)], 'EdgeColor', 'r', 'lineWidth', 4);
+    hold off;
+    saveas(2, sprintf('%s/%d.png',  'tmp1', i)); 
+end
